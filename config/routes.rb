ddc927cc991889eb772423fffa15ac44ab2988f2
fumiya0312 <good.job0312@gmail.com>
2018-09-13 10:17:17 +0900

    Group add
    アソシエーション記述
    カラム記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root  'messages#index'
-  resources :users, only: [:edit, :update]
+  root  'groups#index'
+    resources :users, only: [:index, :edit, :update]
+    resources :groups, only: [:new, :create, :edit, :update] do
+      resources :messages, only: [:index, :create]
+  end
 end
+
+#group controller 作成
+#message,user controller定義
