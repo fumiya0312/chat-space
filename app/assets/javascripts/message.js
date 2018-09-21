e6914a15accb1fe697bde39522561fa21474ef5d
fumiya0312 <good.job0312@gmail.com>
2018-09-21 20:06:35 +0900

    Image.url
    三項演算子、画像の非同期通信

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,13 @@
 $(function() {
   function buildHTML(message) {
+    var image = message.image ?`<img class="main-text__iamge" src="${message.image}">` :""
     var html = `<div class = "main-sub">
                   <div class = "main-name">${message.name}</div>
                   <div class = "main-time">${message.time}</div>
                   <div class = 'main-text'>${message.content}</div>
+                  ${image}
                 </div>`
+
     return html;
   }
   $('#new_message').on('submit', function(e) {
