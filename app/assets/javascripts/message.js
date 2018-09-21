7e7113d9012d81b1e6b089fe5934cf5f65731a84
fumiya0312 <good.job0312@gmail.com>
2018-09-21 19:20:19 +0900

    Auto scroll

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,24 +1,34 @@
 $(function() {
-  // function buildHTML(message) {
-  //   var html
-  // }
+  function buildHTML(message) {
+    var html = `<div class = "main-sub">
+                  <div class = "main-name">${message.name}</div>
+                  <div class = "main-time">${message.time}</div>
+                  <div class = 'main-text'>${message.content}</div>
+                </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var href = window.location.href + '/mesasges'
+    var url = $(this).attr('action');
     $.ajax({
-      url: href,
-      type: "POST",
+      url: url,
+      type: 'POST',
       data:formData,
-      dataType: 'jason',
+      dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data) {
+      console.log("テスト")
       var html = buildHTML(data);
-      $('.messages').append(html);
-      $('f.text_field').val('');
+      $('.main-chat').append(html);
+      $('.form__message').val('');
+      $(".main-chat").animate({scrollTop: $(".main-chat")[0].scrollHeight}, 1000, "swing");
     })
-
+    .fail(function(){
+      alert('error');
+    })
+    return false;
   })
 })
